944bbd42dad3a78c6cd790942c7f39334bf31c96
unknown <noubissibrayan@gmail.com>
2025-02-27 12:33:31 +0100

    deploy

---
+++ index.js
@@ -0,0 +1,54 @@
+//IMPORTS
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+// const ToggleRoute = require('./Endpoints/index').router
+const mongoose = require('mongoose');
+//const corsOptions =  require('./Config/corsOptions')
+const cors = require('cors')
+require('dotenv').config()
+
+
+
+// app.use(credentials);
+
+// Cross Origin Resource Sharing
+// app.use(cors(corsOptions));
+//Encryptment
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//cors & cookies
+
+app.use(cors())
+app.use(cookieParser());
+
+
+
+// //image
+// app.use('/image', express.static(path.join(__dirname,'/Public/image')))
+
+// //API TOGGLE ROUTE
+// app.use('/api', ToggleRoute);
+
+
+//PORT
+const PORT = 5000;
+
+
+app.get('/', (req, res)=>{
+
+    const root = path.join(__dirname, '/index.html')
+    res.setHeader("Content-type", 'text/html');
+    res.status(200).sendFile(root)
+})
+
+mongoose.connect(process.env.DATABASE_URI)
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
